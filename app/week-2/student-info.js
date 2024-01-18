91d8cdd555acd2af2da4a5efd1053de01b603c0f
Farhan Batada <farhan.batada@edu.sait.ca>
2024-01-18 15:57:56 -0700

    Week 2 Assignment

---
+++ app/week-2/student-info.js
@@ -0,0 +1,16 @@
+import Link from 'next/link';
+
+export default function StudentInfo()
+{
+    return (
+        <main>
+            <h1>Name: Farhan Batada</h1>
+
+            <ul>
+                <li>
+                    <Link href="https://github.com/Farhan888132/cprg306-assignments ">Github-Repository</Link>
+                </li>
+            </ul>
+        </main>
+    );
+}
